9ec7ddb71323dab8383ac5764cc09046cfe9de9c
S V <48d9880e@opayq.com>
2015-12-09 14:35:18 +0200

    Added updates for Google Analytics for Kalaam

--- head.php
+++ head.php
@@ -107,7 +107,17 @@ function check_data_change(url)
  return false;
 }
 
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-28615935-2']);
+    _gaq.push(['_trackPageview']);
 
+    (function() {
+	        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		      })();
+
+			  
 </SCRIPT>
 
 </head>
